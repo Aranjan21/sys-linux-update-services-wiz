92ce666b6d7934e281ac3beac1b7017110c7b8a1
A.Ranjan <A.Ranjan@cvent.com>
2018-04-05 23:42:45 -0400

    fixing line 16

--- start_wiz_services.sh
+++ start_wiz_services.sh
@@ -1,1 +1,25 @@
-start_wiz_services.sh
+#start_wiz_services.sh
+#Created by Abhishek Ranjan
+#This script will run on a Jenkins Linux slave and start all the wiz services.
+
+
+cat <<EOF > remote.sh
+# Set the PATH and TERM because piping a script to sshpass via STDIN does not initialize a terminal
+export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
+export TERM=xterm
+ 
+sudo cat services.txt
+failed_service=()
+while read line ;do
+initctl start $line
+serv=`initctl status $line`
+		if [[ $serv != *"start"* ]]; then
+			failed_service+=("$line")
+		fi
+done < "services.txt"
+echo "List of all failed services"$'\n'"$failed_service"
+ 
+EOF
+ 
+# Execute the script on the remote Linux machine
+sshpass -p "$__password__" ssh -o StrictHostKeyChecking=no $__username__@$_address_ < remote.sh
